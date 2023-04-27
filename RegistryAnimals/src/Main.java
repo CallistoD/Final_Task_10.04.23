3fb2d00402ba6d842a5679a5b8c72e3be38b5971
Callisto <125141407+CallistoD@users.noreply.github.com>
2023-04-27 20:25:15 +0300

    Add files via upload

---
+++ RegistryAnimals/src/Main.java
@@ -0,0 +1,11 @@
+import Model.Zoo;
+import Interface.UI;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        UI.run();
+
+    }
+}
